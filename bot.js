a9c3855bffc85c1cba98a0b1436f155aef72ee62
team10001 <41784577+team10001@users.noreply.github.com>
2018-07-31 00:16:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,28 @@
 const Discord = require('discord.js');
+const db = require('quick.db');
 const client = new Discord.Client();
-const prefix = '.'
+const giphy = require('giphy-api')();
+const googl = require('goo.gl');
+const translate = require('google-translate-api');
+const fs = require("fs"); 
+const getYoutubeID = require('get-youtube-id'); 
+const moment = require("moment");
+const { Client, Util } = require('discord.js');
+const UserBlocked = new Set(); 
+const jimp = require('jimp');
+const math = require('math-expression-evaluator'); 
+const stripIndents = require('common-tags').stripIndents;
+const figlet = require('figlet');
+const google = require('google-it'); 
+const queue = new Map(); 
+const zalgo = require('zalgolize');
+const fetchVideoInfo = require('youtube-info');
+const YouTube = require('simple-youtube-api');
+const ytdl = require('ytdl-core');
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+const sql = require("sqlite");
+ const dateFormat = require('dateformat'); 
+ const pretty = require('pretty-ms')
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
