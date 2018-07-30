58ab5237d780ed445c02428ffea7a941a910e659
team10001 <41784577+team10001@users.noreply.github.com>
2018-07-30 18:44:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,7 +36,6 @@ const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
 
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
-client.login(discord_token);
 client.on('ready', function() {
     console.log(`i am ready ${client.user.username}`);
 });
