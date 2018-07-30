bbb2026ccbe314492b27e2d21031fe493261eca1
team10001 <41784577+team10001@users.noreply.github.com>
2018-07-30 18:42:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -312,4 +312,51 @@ For additional help,  `)
                         }
                     });
 
+const developers = ["384435460564451328"]
+
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+      if (message.content.startsWith(prefix + 'ply')) {
+        client.user.setGame(argresult);
+        message.channel.send(`**Ok, playing..** **${argresult}!**`)
+
+    } else
+
+      if (message.content === (prefix + "leave")) {
+        message.guild.leave();
+
+    } else
+
+      if (message.content.startsWith(prefix + 'wt')) {
+        client.user.setActivity(argresult, {type:'WATCHING'});
+        message.channel.send(`**Ok, watching..** **${argresult}!**`)
+
+    } else
+
+     if (message.content.startsWith(prefix + 'ls')) {
+        client.user.setActivity(argresult , {type:'LISTENING'});
+        message.channel.send(`**Ok, listening to..** **${argresult}!**`)
+    } else
+
+     if (message.content.startsWith(adminprefix + 'set-name')) {
+        client.user.setUsername(argresult).then
+        message.channel.send(`**Changing my name to..** **${argresult}!** `)
+
+    } else
+
+     if (message.content.startsWith(prefix + 'set-avatar')) {
+        client.user.setAvatar(argresult);
+        message.channel.send(`**Changing my avatar to..** ${argresult}`);
+
+    } else
+
+    if (message.content.startsWith(prefix + 'set-status')) {
+        client.user.setStatus(argresult)
+        message.channel.send(`**Ok, status changed to..** **${argresult}!**`)
+    }
+
+});
+
 client.login(process.env.BOT_TOKEN);
